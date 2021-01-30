7bc5e1cdf88b9adb302627cde5b7fad3eda0e0a8
onlystard <onlystard@gmail.com>
2021-01-30 13:13:15 +0700

    First Commit

---
+++ application/views/frontend/components/lienhe/index.php
@@ -0,0 +1,75 @@
+<?php echo form_open( base_url()."lien-he"); ?>
+<section>
+	<div class="container">
+		<div class="col-md-7 col-12">
+			<div class="section-article contactpage" style="  padding-left: 20px;">
+				<?php 
+				echo validation_errors();
+				
+				?>
+				<form accept-charset="UTF-8" action="<?php echo base_url() ?>lien-he" id="contact" method="post">
+					<input name="FormType" type="hidden" value="contact">
+					<input name="utf8" type="hidden" value="true">
+					<h1 style="color: black">Liên hệ với chúng tôi</h1>				
+					
+					<div class="form-comment">
+						<div class="row" style="padding-left: 14px;">
+							<div class="col-md-4 col-12">
+								<div class="form-group" style="width: 200px;">
+									<label for="name"><em> Họ tên</em><span class="required">*</span></label>
+									<input id="name" name="fullname" type="text" value="" class="form-control">
+								</div>
+							</div>
+							<div class="col-md-4 col-12">
+								<div class="form-group" style="width: 200px;">
+									<label for="email"><em> Email</em><span class="required">*</span></label>
+									<input id="email" name="email" class="form-control" type="email" value="">
+								</div>
+							</div>
+							<div class="col-md-4 col-12">
+								<div class="form-group" style="width: 200px;">
+									<label for="phone"><em> Số điện thoại</em><span class="required">*</span></label>
+									<input type="number" id="phone" class="form-control" name="phone">
+
+								</div>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="message"><em> Tiêu đề</em><span class="required">*</span></label>
+							<textarea id="message" name="title" class="form-control custom-control" rows="2"></textarea>
+						</div>
+						<div class="form-group">
+							<label for="message"><em> Lời nhắn</em><span class="required">*</span></label>
+							<textarea id="message" name="content" class="form-control custom-control" rows="5"></textarea>
+						</div>
+						<button type="submit" class="btn-update-order" >Gửi nhận xét</button>
+
+					</div>
+				</form>
+			</div>
+		</div>
+		<div class="col-md-4 col-12">
+			<div class="f-contact" style="
+			padding-left: 32px;
+			">
+			<h1 style="color: black">Thông tin liên hệ</h1>
+			<ul class="list-unstyled">
+				<li class="clearfix">
+					<i class="fa fa-map-marker fa-1x" style="color:#0f9ed8; padding: 20px; "></i>
+					<span style="color: black">02 Tran Dai Nghia, Da Nang</span><br>
+				</li>
+				<li class="clearfix">
+					<i class="fa fa-phone fa-1x" style="color:#0f9ed8;padding: 20px;  "></i>
+					<span style="color: black">0763882935</span>
+				</li>
+				<li class="clearfix">
+					<i class="fa fa-envelope fa-1x " style="color:#0f9ed8; padding: 20px; "></i>
+					<span style="color: black"><a href="mailto:sale.24hstore@gmail.com">sale.smartstore@gmail.com</a></span>
+				</li>
+			</ul>
+		</div>
+
+	
+</div>
+
+</section>
